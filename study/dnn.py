12a5cef87494e3481f8b33906b86dbbd646389f7
ssgkmd <tlagyqls7@naver.com>
2019-01-23 17:57:18 +0900

    mnist_linear

--- study/dnn.py
+++ study/dnn.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 import numpy as np
 
 data = np.loadtxt('./data.csv', delimiter=',', unpack=True, dtype='float32')
-x_data = np.transpose(data[0:2])
-y_data = np.transpose(data[2:])
+x_data = data[:,0:-1]
+y_data = data[:,[-1]]
 
 global_step = tf.Variable(0, trainable=False, name='global_step')
 
